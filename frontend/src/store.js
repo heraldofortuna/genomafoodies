d1e33ba7930b40eef18122fa57ec77753e97c84f
heraldofortuna <heraldofortuna@gmail.com>
2021-12-28 12:45:18 -0500

    Creo un store con data dummy

---
+++ frontend/src/store.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const genomafoodiesItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    location: "Buy ingredients to prepare dinner",
+    food_type: "Marina",
+    score: 2,
+    visited: true,
+  },
+  {
+    id: 2,
+    title: "Study",
+    location: "Read Algebra and History textbook for the upcoming test",
+    food_type: "Marina",
+    score: 4,
+    visited: false,
+  },
+  {
+    id: 3,
+    title: "Sammy's books",
+    location: "Go to library to return Sammy's books",
+    food_type: "Marina",
+    score: 1,
+    visited: true,
+  },
+  {
+    id: 4,
+    title: "Article",
+    location: "Write article on how to use Django with React",
+    food_type: "Marina",
+    score: 3,
+    visited: false,
+  },
+];
+
+export default genomafoodiesItems;
