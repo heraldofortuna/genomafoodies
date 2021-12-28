f152a5ed725c5b1a2528c72a7732da9f72030b56
heraldofortuna <heraldofortuna@gmail.com>
2021-12-28 01:28:52 -0500

    El modelo de Genomafoodies fue registrado y guardado

--- backend/genomafoodies/admin.py
+++ backend/genomafoodies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Genomafoodies
+
+
+class GenomafoodiesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'ubication', 'food_type', 'score', 'visited')
+
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
--- backend/genomafoodies/models.py
+++ backend/genomafoodies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genomafoodies(models.Model):
+    title = models.CharField(max_length=120)
+    ubication = models.TextField()
+    food_type = models.TextField()
+    score = models.IntegerField()
+    visited = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
