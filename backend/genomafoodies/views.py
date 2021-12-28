cbb6f5fb75800984b7f14be1ba0a02ef65c0bad5
heraldofortuna <heraldofortuna@gmail.com>
2021-12-28 02:06:51 -0500

    Las vistas y las urls fueron creadas con éxito

--- backend/genomafoodies/views.py
+++ backend/genomafoodies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import GenomafoodiesSerializer
+from .models import Genomafoodies
 
 # Create your views here.
+
+
+class GenomafoodiesView(viewsets.ModelViewSet):
+    serializer_class = GenomafoodiesSerializer
+    queryset = Genomafoodies.objects.all()
